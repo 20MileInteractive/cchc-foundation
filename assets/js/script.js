5d045aef1a9cc66a775f6edcfe56184dafedad17
David Fontes <dfontes@20miletech.com>
2013-08-28 14:09:00 -0400

    Ajax script

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+$(document).foundation();
+
+$(document).ready(function() {
+    $("#gifts-of-stocks-or-securities").load('/ways-to-give/gifts-of-stocks-or-securities');
+    
+    $( ".ajax-content" ).each(function( index ) {
+    	var pageToLoad = window.location.pathname + $(this).attr('id');
+		$(this).load(pageToLoad);
+	});
+});
