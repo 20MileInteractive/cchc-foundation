071aacb1aa505b930932d3703eb81b2bb9a0e948
David Fontes <dfontes@20miletech.com>
2013-09-23 08:10:19 -0400

    ie8 gist added

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,9 @@ $(document).ready(function() {
     $("#gifts-of-stocks-or-securities").load('/ways-to-give/gifts-of-stocks-or-securities');
     
     $( ".ajax-content" ).each(function( index ) {
-    	var pageToLoad = window.location.pathname + $(this).attr('id');
+    	var pageToLoad = window.location.pathname;
+    	if(pageToLoad.charAt(pageToLoad.length-1) != '/'){pageToLoad+='/'};
+    	pageToLoad = pageToLoad + $(this).attr('id');
 		$(this).load(pageToLoad);
 	});
 	$('.futurepast').first().show();
